777d6cde99bc25fb4a95bfcad4fe570e8bd35cee
potatowaffles4 <xionaxx4@gmail.com>
2025-01-12 17:21:50 +0000

    try

--- api/main.py
+++ api/main.py
@@ -70,7 +70,19 @@ async def artwork_get(id: str = ""):
             "name": "David Hockney",
             "country": "United Kingdom",
             "image_url": "https://observer.com/wp-content/uploads/sites/2/2018/04/gettyimages-615380796-e1524080376439.jpg"
-        }
+        },
+        {
+            "id": "3",
+            "name": "Claude Monet",
+            "country": "France",
+            "image_url": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F3%2F33%2FClaude_Monet_1899_Nadar.jpg&f=1&nofb=1&ipt=3b88e2ea59efbbd851908f0abbd586962b6b30d0905c1d1bc9377fbe23049299&ipo=images"
+        },
+        {
+            "id": "4",
+            "name": "Vincent Van Gogh",
+            "country": "Netherlands",
+            "image_url": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F60%2FVincent_Willem_van_Gogh_107.jpg&f=1&nofb=1&ipt=2a428db71f93544de6cf9bba3c4f1f61f9e79cd7f6b0fd12852669fafbab8c7d&ipo=images"
+        },
     ]
 
     for artist in artists:
