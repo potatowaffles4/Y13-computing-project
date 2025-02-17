2d18afca5ae55f8194979f300dd682641870b216
Brett Jackson <github@bcjmail.co.uk>
2025-02-17 17:03:48 +0000

    try

--- start-api.cmd
+++ start-api.cmd
@@ -1,2 +1,7 @@
+python3 -m venv ./.venv
+. ./.venv/bin/activate
+
 cd api
+pip install -r requirements.txt
+
 python3 -m fastapi dev main.py
--- start-app.cmd
+++ start-app.cmd
@@ -1,4 +1,8 @@
 cd app
 path=%path%;C:\Users\School\AppData\Local\fnm_multishells\24668_1736081946498
+
+npm ci
+
 npm start
 pause
+
